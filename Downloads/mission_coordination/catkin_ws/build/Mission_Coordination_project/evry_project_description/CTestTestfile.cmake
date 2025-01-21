58148f106492ae08578402375065d7ee88692aae
melk2024 <167713679+melk2024@users.noreply.github.com>
2025-01-21 04:48:47 +0100

    Delete Downloads/mission_coordination directory

--- Downloads/mission_coordination/catkin_ws/build/Mission_Coordination_project/evry_project_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/catkin_ws/src/Mission_Coordination_project/evry_project_description
-# Build directory: /home/user/catkin_ws/build/Mission_Coordination_project/evry_project_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
